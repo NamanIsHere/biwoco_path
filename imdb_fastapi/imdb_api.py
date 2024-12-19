57d0220e884d840c38e51347aa17c54b1010339e
NamanIsHere <namtran.su@gmail.com>
2024-12-19 17:20:13 +0700

    do some small changes

--- imdb_fastapi/imdb_api.py
+++ imdb_fastapi/imdb_api.py
@@ -4,6 +4,7 @@ This module sets up the FastAPI app and includes movie-related endpoints.
 """
 
 import sys
+import json
 from fastapi import FastAPI
 from imdb_fastapi.routes.route import endPoints
 
@@ -20,13 +21,15 @@ def root():
     """
     return{
         'message': 'Fast API is running!',
-        'List of regions code use for API':"""
-        AF, AL, AS, AI, AR, AM, AU, AT, BD, BB, BY, BE, BJ, BM,
-        BR, KH, CM, CA, CF, CL, CN, CC, CO, CR, HR, CU, CW, CY, 
-        CZ, DK, DM, DO, EC, EG, GQ, EE, FI, FR, GF, GA, GM, GE, 
-        DE, GR, GN, GW, HK, HU, IS, IN, ID, IR, IE, IL, IT, JP, 
-        KZ, KW, LV, LB, LR, LY, LT, LU, MO, MY, MV, MH, MX, ME, 
-        MM, NP, NL, NZ, NI, NG, NO, PK, PY, PE, PH, PL, PT, PR, 
-        QA, RO, RU, SA, RS, SG, SK, SI, ZA, KR, ES, LK, SE, CH, 
-        TW, TH, TR, UA, AE, GB, US, UY, VE, VN"""
+        'List of regions code use for API':
+        """\
+AF, AL, AS, AI, AR, AM, AU, AT, BD, BB, BY, BE, BJ, BM,\
+BR, KH, CM, CA, CF, CL, CN, CC, CO, CR, HR, CU, CW, CY,\
+CZ, DK, DM, DO, EC, EG, GQ, EE, FI, FR, GF, GA, GM, GE,\
+DE, GR, GN, GW, HK, HU, IS, IN, ID, IR, IE, IL, IT, JP,\
+KZ, KW, LV, LB, LR, LY, LT, LU, MO, MY, MV, MH, MX, ME,\
+MM, NP, NL, NZ, NI, NG, NO, PK, PY, PE, PH, PL, PT, PR,\
+QA, RO, RU, SA, RS, SG, SK, SI, ZA, KR, ES, LK, SE, CH,\
+TW, TH, TR, UA, AE, GB, US, UY, VE, VN
+        """
         }
